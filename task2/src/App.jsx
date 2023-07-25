c0b4a20aab80163aff94843a0f458cadb506c0f8
ravi-vish-au50 <ravivish166@gmail.com>
2023-07-25 08:17:29 +0530

    data calling from api feching

--- task2/src/App.jsx
+++ task2/src/App.jsx
@@ -1,55 +1,27 @@
-// import React, { useState } from "react";
-// import "./App.css";
-
-// const App = () => {
-//   const [users, setUser] = useState([]);
-//   const loadUsers = async () => {
-//     console.log("before");
-//     const response = await fetch("http://api.github.com/users");
-//     const jsonrersponse = await response.json();
-//     setUser(jsonrersponse);
-//   };
-//   return (
-//     <>
-//       <div className="App">
-//         <h1>Hello all</h1>
-//         <button onClick={loadUsers}>Get Data</button>
-//         <h2>Users:</h2>
-//         <ul>
-//           {users.map(({ id, login, avatar_url }) => (
-//             <li key={id}>
-//               Name : {login}, Avatar: {avatar_url}{" "}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default App;
-
-
-
-
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import "./App.css";
 
 function App() {
+  // State to manage the current page number for API query
   const [query, setQuery] = useState(1);
+  // State to store the fetched data from the API
   const [data, setData] = useState([]);
+    // State to indicate whether the API request is in progress
   const [isLoading, setIsLoading] = useState(false);
-
+  // Function to fetch data from the API
   const fetchApi = async () => {
     setIsLoading(true);
-    const res = await axios(`https://reqres.in/api/users?page=${query}`);
-
-    setData((prevData) => {
-      return [...prevData, ...res.data.data];
-    });
-    setIsLoading(false);
+    try {
+      const res = await axios(`https://reqres.in/api/users?page=${query}`);
+      setData((prevData) => [...prevData, ...res.data.data]);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setIsLoading(false);
+    }
   };
-
+ // Function to handle "FetchApi" button click
   const handleNextQuery = () => {
     setQuery((prev) => prev + 1);
     fetchApi();
@@ -57,66 +29,38 @@ function App() {
 
   return (
     <>
-      <header className="header w-full">
-        <nav className="nav">
-          <h1 className="brand">Sasta Api</h1>
-          {query === 3 ? (
-            <>
-              <a href="/" className="btn">
-                Api Limit Reached Reset
-              </a>
-            </>
-          ) : (
-            <>
-              <button className="btn" onClick={handleNextQuery}>
-                FetchApi
-              </button>
-            </>
-          )}
-        </nav>
+      <header className="header">
+        <h1 className="brand">
+          Create a web application using create-react-app for fetching an api
+          for data
+        </h1>
+        <button className="btn" onClick={handleNextQuery} disabled={isLoading}>
+          FetchApi
+        </button>
       </header>
 
-      {data.length === 0 && (
-        <section className="loaderSection">
-          <p className="para">
-            This is the same loader that will show when you click on the
-            fetchapi,<br />I have usedd it to show on the home screen just so we can see it
-          </p>
+      <section className="dataSection">
+        {data.map((item) => (
+          <div key={item.id} className="data">
+            <img className="img" src={item.avatar} alt="" />
+            <div className="text">
+              <p className="name text-md">
+                {item.first_name} {item.last_name}
+              </p>
+              <p className="email">{item.email}</p>
+            </div>
+          </div>
+        ))}
+      </section>
+
+      {isLoading && (
+        <div className="loaderSection">
           <div className="loader">
             <div className="circle"></div>
             <div className="line-1"></div>
             <div className="line-2"></div>
           </div>
-        </section>
-      )}
-
-      {isLoading ? (
-        <>
-          <section className="loaderSection">
-            <div className="loader">
-              <div className="circle"></div>
-              <div className="line-1"></div>
-              <div className="line-2"></div>
-            </div>
-          </section>
-        </>
-      ) : (
-        <>
-          <section className="dataSection">
-            {data &&
-              data.map((item, index) => (
-                <div key={index} className="data">
-                  <img className="img" src={item.avatar} alt="" />
-                  <div className="text">
-                    <p className="name text-md">
-                      {item.first_name} {item.last_name}
-                    </p>
-                    <p className="email">{item.email}</p>
-                  </div>
-                </div>
-              ))}
-          </section>
-        </>
+        </div>
       )}
     </>
   );
